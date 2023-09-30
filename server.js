2416e1ea9c2c6da1065828b2ecd0ed7d278a30d5
sapphire <chidinmaakunwa@gmail.com>
2023-09-30 21:46:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ const app = express();
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
   }
 })
 
