df5ab0872738e6668bff366de534d3b46aa346da
Chidinma Judith Akunwa <chidinmaakunwa@gmail.com>
2023-10-01 16:44:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ app.get('/videos/:id', (req, res) => {
         const filePath = video.path;
 
         // Set the content type header
-        res.set('Content-Type', 'video/mp4');
+        res.set('Content-Type', 'video/webm');
 
         // Create a read stream from the file and pipe it to the response
         const stream = fs.createReadStream(filePath);
